813a67379c4855af8312eb3869f3484bd926df76
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2023-01-16 09:47:22 +0530

    Added extension

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" == "main" ];
-then
-    #plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/AppCenterApp/Info.plist
-fi
+#if [ "$APPCENTER_BRANCH" == "main" ];
+#then
+#    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/AppCenterApp/Info.plist
+#fi
