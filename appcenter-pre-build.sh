b5a5884c0c2424d29483a7843c00f39179f43b90
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2023-01-17 14:51:05 +0530

    clear the cached package version

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-# Example: Change bundle name of an iOS app for non-production
+# Delete Derived Data folder to clear the cached package version.
+# NOTE: Package version rule is based on Major release.
+# w.r.f: step name 'Update Swift package and Run unit test'
+rm -rf ~/Library/Developer/Xcode/DerivedData/
+
+
+# Delete Package.resolved file.
+# 'Derived Data' and 'Package.resolved' won't let the package to update
+# to the latest version while executing package update from terminal.
+# Always Cached data will be rendered. Hence deleting 'Derived Data' and 'Package.resolved'.
+# w.r.f: step name 'Update Swift package and Run unit test'
+rm -rf ~/AppCenterApp.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+        
 #if [ "$APPCENTER_BRANCH" == "main" ];
 #then
 #    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/AppCenterApp/Info.plist
